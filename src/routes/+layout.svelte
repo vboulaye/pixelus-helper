e1cfb333b31f2cca50e944b95053c06b234909c3
Vincent Boulaye <vincent.boulaye@orange.com>
2025-08-02 23:01:18 +0200

    search by artists

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<nav>
+  <ul>
+    <li><a href="/">By Dictionary</a></li>
+    <li><a href="/artists">By Artists</a></li>
+  </ul>
+</nav>
+
+<slot />
+
+<style>
+nav {
+  background: #ff7900;
+  padding: 1rem;
+  font-family: Helvetica, Arial, sans-serif;
+}
+ul {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, Arial, sans-serif;
+}
+li {
+  display: inline;
+}
+a {
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+  font-family: Helvetica, Arial, sans-serif;
+}
+a:hover {
+  color: #0070f3;
+}
+</style>
